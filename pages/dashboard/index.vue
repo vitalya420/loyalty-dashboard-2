168260b7b364b63868cb66f0f80bb35e4fe88fe2
Vitaliy <v.izhakivskiy@gmail.com>
2024-11-01 22:57:25 +0200

    dashboard card

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>index of dashboard</div>
+  <Card title="Dashboard">
+    Dashboard page will be individual
+  </Card>
 </template>
 
 <script setup>
+import Card from '~/components/cards/Card.vue';
 definePageMeta({
   layout: 'dashboard',
 });
