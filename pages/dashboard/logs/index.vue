f203b9bd4e67e755edecb6d37db9706754128ae0
Vitaliy <v.izhakivskiy@gmail.com>
2024-11-01 22:56:28 +0200

    forgotten page

---
+++ pages/dashboard/logs/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Card title="Logs" subtitle="View what you staff and clients do">
+    yea
+  </Card>
+</template>
+
+
+<script setup>
+import Card from '~/components/cards/Card.vue';
+definePageMeta({
+  layout: 'dashboard',
+});
+</script>
