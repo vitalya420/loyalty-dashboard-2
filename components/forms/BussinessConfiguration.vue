579f62cb9a08e70e856cd169ac1605e36d6dc885
Vitaliy <v.izhakivskiy@gmail.com>
2024-11-02 20:48:16 +0200

    forms components

--- components/forms/BussinessConfiguration.vue
+++ components/forms/BussinessConfiguration.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>bussines config</div>
+  <div class="form-column padding-y-5px">
+    <div class="input-group-row space-between">
+      <label for="program-enabled">Bonus program enabled:</label>
+      <input type="checkbox" class="switch" id="program-enabled">
+    </div>
+    <div class="input-group-row space-between">
+      <label for="stamps-enabled">Stamps system:</label>
+      <input type="checkbox" class="switch" id="stamps-enabled">
+    </div>
+    <div class="input-group-row space-between">
+      <label for="coupons-enabled">Coupons:</label>
+      <input type="checkbox" class="switch" id="coupons-enabled">
+    </div>
+    <div class="input-group-row space-between">
+      <label for="referals-enabled">Referal system reward:</label>
+      <input type="checkbox" class="switch" id="referals-enabled">
+    </div>
+    <div class="input-group-row space-between">
+      <label for="menu-enabled">Menu website:</label>
+      <input type="checkbox" class="switch" id="menu-enabled">
+    </div>
+    <div class="input-group-row space-between">
+      <label for="tz">Default Timezone:</label>
+      <input type="text" class="switch" id="tz">
+    </div>
+  </div>
 </template>
--- components/forms/BussinessInformation.vue
+++ components/forms/BussinessInformation.vue
@@ -1,3 +1,62 @@
 <template>
-  <div>Business information</div>
+  <div class="form-column padding-y-5px">
+    <div class="input-group-column">
+      Update Logo:
+      <div class="logo">
+        <div class="logo-backdrop">
+          <button class="update-logo-button">Click to update</button>
+        </div>
+      </div>
+    </div>
+    <div class="input-group-column">
+      <label for="name">Business name:</label>
+      <input type="text" placeholder="Coffee shop" class="text-input" id="name">
+    </div>
+    <div class="input-group-column">
+      <label for="desc">Description: </label>
+      <textarea placeholder="Write about your business" rows="5" class="text-input" id="desc"></textarea>
+    </div>
+    <div>
+      <button class="button success">Save</button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.logo {
+  width: 210px;
+  aspect-ratio: 3;
+  height: auto;
+  background-image: url("https://placehold.co/300x100");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
+
+.logo-backdrop {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transition: background-color 0.5s ease;
+  transition: opacity 0.5s ease;
+  opacity: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo-backdrop:hover {
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 1;
+}
+
+.update-logo-button {
+  cursor: pointer;
+  border: none;
+  background: transparent;
+  color: white;
+  font-weight: bold;
+  width: 100%;
+  height: 100%;
+}
+</style>
